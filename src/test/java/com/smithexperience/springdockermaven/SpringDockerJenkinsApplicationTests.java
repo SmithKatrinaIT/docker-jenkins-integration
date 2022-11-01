61f624569ca71555b18422dbf2c680361d543121
katrina smith <ksmith10@ggc.edu>
2022-11-01 07:50:06 -0400

    first commit

---
+++ src/test/java/com/smithexperience/springdockermaven/SpringDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smithexperience.springdockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
