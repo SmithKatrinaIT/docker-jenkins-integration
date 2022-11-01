66fe13e1ea04efa702232ff1517096bd7b9aff3c
katrina smith <ksmith10@ggc.edu>
2022-11-01 08:07:45 -0400

    new class added

---
+++ src/main/java/com/smithexperience/springdockermaven/SyncJenkinsGitHub.java
@@ -0,0 +1,13 @@
+package com.smithexperience.springdockermaven;
+
+public class SyncJenkinsGitHub {
+	
+	public static void main(String args []) {
+	
+		System.out.println("Hello World");
+
+	
+
+	}
+
+}
